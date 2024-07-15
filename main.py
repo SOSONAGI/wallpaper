ceca5731ce81a5d1cb131d7b8eff954f513e523b
SUN YOUNG HWANG <103930629+SOSONAGI@users.noreply.github.com>
2024-07-15 12:07:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ import timm
 import base64
 from typing import List, Dict
 import pandas as pd
-from huggingface_hub import hf_hub_download
 
 class Config:
     num_classes = 19
     class_names = ['오염', '녹오염', '면불량', '들뜸', '이음부불량', '터짐', '피스', '오타공', '석고수정', '가구수정', '몰딩수정', '틈새과다', '꼬임', '울음', '걸레받이수정', '반점', '훼손', '창틀,문틀수정', '곰팡이']
-
 config = Config()
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DINO(nn.Module):
     def __init__(self, freeze=False):
         super(DINO, self).__init__()
-        self.model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14')
+        self.model = torch.hub.load('facebookresearch/dinov2', f'dinov2_vitl14')
         self.freeze = freeze
         if self.freeze:
             for para in self.model.parameters():
                 para.requires_grad = False
+
         self.clf = nn.Sequential(
             nn.Linear(self.model.embed_dim, 512),
             nn.BatchNorm1d(512),
@@ -55,18 +55,23 @@ val_transform = T.Compose([
     T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
-# 허깅페이스에서 모델 가중치 다운로드
-model_path = hf_hub_download(repo_id="sosoai/dino_checkpoint", filename="best3.pt")
+# 허깅페이스 레포지토리에서 가중치 불러오기
+from transformers import AutoConfig, AutoModel
+
+huggingface_model = AutoModel.from_pretrained('sosoai/dino_checkpoint')
+huggingface_state_dict = huggingface_model.state_dict()
 
 model = DINO().to(device)
-model.load_state_dict(torch.load(model_path, map_location=device))
+model.load_state_dict(huggingface_state_dict, strict=False)  # 기존 모델의 가중치에 허깅페이스 가중치를 덮어씌움
 model.eval()
 
 def predict(image):
     image_transformed = val_transform(image).unsqueeze(0).to(device)
+
     with torch.no_grad():
         output = model(image_transformed)
         predicted_class_idx = torch.argmax(output, dim=1).item()
+
     return predicted_class_idx
 
 class_counter = {class_name: 0 for class_name in config.class_names}
@@ -78,14 +83,10 @@ def classify_and_save_file(uploaded_file, target_dir):
     target_folder = os.path.join(target_dir, class_name)
     os.makedirs(target_folder, exist_ok=True)
 
-    # Increment the class counter and generate the new file name
     class_counter[class_name] += 1
     new_filename = f"{class_name}{class_counter[class_name]}.png"
 
-    # 이미지 크기를 512x512로 변경
     resized_image = image.resize((512, 512), Image.BICUBIC)
-
-    # 이미지를 PNG 형식으로 저장
     image_path = os.path.join(target_folder, new_filename)
     resized_image.save(image_path, 'PNG')
 
